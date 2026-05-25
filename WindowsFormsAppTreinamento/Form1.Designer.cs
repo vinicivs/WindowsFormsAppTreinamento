76a42b77837f9f3429338321cb86c392d76172ae
vinicivs <vinicivs@gmail.com>
2026-05-25 12:26:40 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsAppTreinamento/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsAppTreinamento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPesquisar = new System.Windows.Forms.Label();
+            this.mskCep = new System.Windows.Forms.MaskedTextBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.gbxEndereco = new System.Windows.Forms.GroupBox();
+            this.dgvCep = new System.Windows.Forms.DataGridView();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.gbxEndereco.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCep)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPesquisar
+            // 
+            this.lblPesquisar.AutoSize = true;
+            this.lblPesquisar.Location = new System.Drawing.Point(12, 9);
+            this.lblPesquisar.Name = "lblPesquisar";
+            this.lblPesquisar.Size = new System.Drawing.Size(53, 13);
+            this.lblPesquisar.TabIndex = 0;
+            this.lblPesquisar.Text = "Pesquisar";
+            // 
+            // mskCep
+            // 
+            this.mskCep.Location = new System.Drawing.Point(71, 6);
+            this.mskCep.Name = "mskCep";
+            this.mskCep.Size = new System.Drawing.Size(100, 20);
+            this.mskCep.TabIndex = 1;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(177, 6);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnPesquisar.TabIndex = 2;
+            this.btnPesquisar.Text = "&Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            // 
+            // gbxEndereco
+            // 
+            this.gbxEndereco.Controls.Add(this.dgvCep);
+            this.gbxEndereco.Location = new System.Drawing.Point(12, 35);
+            this.gbxEndereco.Name = "gbxEndereco";
+            this.gbxEndereco.Size = new System.Drawing.Size(571, 221);
+            this.gbxEndereco.TabIndex = 3;
+            this.gbxEndereco.TabStop = false;
+            this.gbxEndereco.Text = "Endereços:";
+            // 
+            // dgvCep
+            // 
+            this.dgvCep.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCep.Location = new System.Drawing.Point(6, 19);
+            this.dgvCep.Name = "dgvCep";
+            this.dgvCep.Size = new System.Drawing.Size(559, 196);
+            this.dgvCep.TabIndex = 0;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(258, 6);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(339, 6);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 5;
+            this.btnSair.Text = "&Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 270);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.gbxEndereco);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.mskCep);
+            this.Controls.Add(this.lblPesquisar);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CEP - Pesquisar";
+            this.gbxEndereco.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCep)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPesquisar;
+        private System.Windows.Forms.MaskedTextBox mskCep;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.GroupBox gbxEndereco;
+        private System.Windows.Forms.DataGridView dgvCep;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
